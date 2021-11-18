bdc7808f8f011ba443450c635cc31d516a64b1a3
paragbramh <paragbramh@gmail.com>
2021-11-18 21:50:13 +0530

    Repo to style React App and make it responsive as well

---
+++ Make me Responsive/Netflix movie App/Readme.md
@@ -0,0 +1,4 @@
+Hey This is the simple React App for my top 5 Fav
+Netflix series !
+
+If you can make it look attractive and make is possible for me to view in all type of devices (Resposnive) then you are a Champ Contributor !
---
+++ Make me Responsive/Netflix movie App/netflix_app/src/Sdata.jsx
@@ -0,0 +1,38 @@
+const Sdata = [
+    {   
+        id:1,
+        imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLoyEOiT3Dc3DJ9RgUw0M37XQ_b6uCNRQrPQ&usqp=CAU",
+        title:"A Netlfix Original Series",
+        sname:"Dark",
+        link:"https://www.netflix.com/in/title/80100172"
+    },
+    {
+        id:2,
+        imgsrc:"https://specials-images.forbesimg.com/imageserve/5d30c8e5eab9270008f9bd2a/960x0.jpg?fit=scale",
+        title:"A Netlfix Original Series",
+        sname:"Money Heist",
+        link:"https://www.netflix.com/in/title/80192098"
+    },
+    {   id:3,
+        imgsrc:"https://upload.wikimedia.org/wikipedia/en/1/11/Narcos_season_3.png",
+        title:"A Netlfix Original Series",
+        sname:"Narcos",
+        link:"https://www.netflix.com/in/title/80025172"
+    },
+    {
+        id:4,
+        imgsrc:"https://i.ytimg.com/vi/ceqOTZnhgY8/maxresdefault.jpg",
+        title:"A Netlfix Original Series",
+        sname:"Breaking Bad",
+        link:"https://www.netflix.com/in/title/70143836"
+    },
+    {
+        id:5,
+        imgsrc:"https://i2.wp.com/feminisminindia.com/wp-content/uploads/2018/07/sacred-games-season-2-netflix.jpg?fit=1280%2C720&ssl=1",
+        title:"A Netlfix Original Series",
+        sname:"Sacred Games",
+        link:"https://www.netflix.com/in/title/80115328"
+    }
+]
+
+export default Sdata;
